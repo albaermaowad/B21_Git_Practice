6b20a849f45d9275b26dd3c295fc1c8dd321e0ed
albaermaowad <albaermaowad@gmail.com>
2021-04-25 23:15:48 -0400

    testing my git

--- src/test/java/HelloTest/Java.java
+++ src/test/java/HelloTest/Java.java
@@ -3,4 +3,9 @@ package HelloTest;
 public class Java {
 
 // testing my awesome hello
+
+    public static void main(String[] args) {
+
+        System.out.println("Test my awesome test");
+    }
 }
